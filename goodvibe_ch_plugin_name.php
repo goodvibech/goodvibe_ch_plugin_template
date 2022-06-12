1c1cb14ce6300395f9f7f4646dfc5bc68ffb002b
Dax Castellón Meyrat <dax@daxcastellon.dev>
2022-06-12 21:29:33 +0200

    Simplify code

--- goodvibe_ch_plugin_name.php
+++ goodvibe_ch_plugin_name.php
@@ -22,53 +22,10 @@ if ( !defined('ABSPATH') ) exit;
 // Currently plugin version.
 define( 'GOODVIBE_CH_PLUGIN_NAME', '1.0.0' );
 
-class Goodvibe_CH_Plugin_Name {
-    
-	/**
-	 * Instance of the object.
-	 *
-	 * @since  1.0.0
-	 * @static
-	 * @access public
-	 * @var null|object
-	 */
-	public static $instance = null;	
+class GoodvibePluginName {	
 	
-	/**
-	 * Access the single instance of this class.
-	 *
-	 * @since  1.0.0
-	 * @return Goodvibe_CH_Plugin_Name
-	 */
-	public static function get_instance() {
-		if ( null === self::$instance ) {
-				self::$instance = new self();
-		}
-		return self::$instance;
-	}
-	
-	/**
-	 * Constructor
-	 * Add the custom functions here.
-	 *
-	 * @since  1.0.0
-	 * @return Goodvibe_CH_Plugin_Name
-	 */
-	private function __construct(){
-		if( !is_admin() ){
-				add_action( 'wp_footer', array( $this, 'example_function') );
-		}
-	}
-	
-	/**
-	 * Example function.
-	 * @return void
-	 */
-	public function example_function(){
-		echo "Example function";
-	}
+  // add custom functions here.
 		
 }
 
-// Init
-Goodvibe_CH_Plugin_Name::get_instance();
+$goodvibePluginName = new GoodvibePluginName();
